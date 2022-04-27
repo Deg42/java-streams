fefa2c52c7f209d9a2cb5ef7c6b90276122bcb89
Juan Antonio González <jaglezrivera@gmail.com>
2022-04-27 14:23:10 +0200

    creating streams from strings

---
+++ src/main/java/es/fpdual/streamCreation/StreamCreationFromStrings.java
@@ -0,0 +1,26 @@
+package es.fpdual.streamCreation;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class StreamCreationFromStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // String
+        String languages = "HTML, CSS, JAVASCRIPT, JAVA C++, C#, RUBY";
+        Pattern.compile(", ")
+                .splitAsStream(languages)
+                .forEach(System.out::println);
+
+        // System Input
+        String input = sc.nextLine();
+        IntStream charStream = input.chars();
+        charStream.forEach(System.out::println);
+
+        charStream.filter(ch -> !Character.isDigit((char) ch)
+                && !Character.isWhitespace((char) ch))
+                .forEach(ch -> System.out.print((char) ch));
+    }
+}
